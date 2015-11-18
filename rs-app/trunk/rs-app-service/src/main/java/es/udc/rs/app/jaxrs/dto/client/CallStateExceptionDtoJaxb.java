256103dbe047dd3ec126a4446881db998b8755f3
o.blanco <o.blanco@95be2163-d335-43e2-8a67-876141709623>
2015-11-18 16:47:35 +0000

    [Service] Nuevas excepciones y mappers
    git-svn-id: https://svn.fic.udc.es/grao4/ia/15-16/ia012@32 95be2163-d335-43e2-8a67-876141709623

--- rs-app/trunk/rs-app-service/src/main/java/es/udc/rs/app/jaxrs/dto/client/CallStateExceptionDtoJaxb.java
+++ rs-app/trunk/rs-app-service/src/main/java/es/udc/rs/app/jaxrs/dto/client/CallStateExceptionDtoJaxb.java
@@ -1,5 +1,38 @@
 package es.udc.rs.app.jaxrs.dto.client;
 
-public class CallStateExceptionDtoJaxb {
+import javax.xml.bind.annotation.XmlElement;
+import javax.xml.bind.annotation.XmlRootElement;
+import javax.xml.bind.annotation.XmlType;
 
+@XmlRootElement(name = "CallStateException")
+@XmlType(name="callStateExceptionType", propOrder = {"clientId", "callId"})
+public class CallStateExceptionDtoJaxb {
+		@XmlElement(required = true)
+		private Long clientId;
+		@XmlElement(required = true)
+		private Long callId;
+
+		public CallStateExceptionDtoJaxb() {
+		}
+		
+		public CallStateExceptionDtoJaxb(Long clientId, Long callId) {
+			this.clientId = clientId;
+			this.callId = callId;
+		}
+
+		public Long getClientId() {
+			return clientId;
+		}
+
+		public void setClientId(Long clientId) {
+			this.clientId = clientId;
+		}
+
+		public Long getCallId() {
+			return callId;
+		}
+
+		public void setCallId(Long callId) {
+			this.callId = callId;
+		}
 }
--- rs-app/trunk/rs-app-service/src/main/java/es/udc/rs/app/jaxrs/exceptions/CallStateExceptionMapper.java
+++ rs-app/trunk/rs-app-service/src/main/java/es/udc/rs/app/jaxrs/exceptions/CallStateExceptionMapper.java
@@ -1,5 +1,21 @@
 package es.udc.rs.app.jaxrs.exceptions;
 
-public class CallStateExceptionMapper {
+import javax.ws.rs.core.Response;
+import javax.ws.rs.ext.ExceptionMapper;
+
+import es.udc.rs.app.exceptions.CallStateException;
+import es.udc.rs.app.jaxrs.dto.client.CallStateExceptionDtoJaxb;
+import es.udc.rs.app.jaxrs.dto.client.InstanceNotFoundExceptionDtoJaxb;
+import es.udc.rs.app.jaxrs.dto.client.RemoveClientExceptionDtoJaxb;
+
+public class CallStateExceptionMapper implements ExceptionMapper<CallStateException> {
+
+	@Override
+	public Response toResponse(CallStateException ex) {
+		return Response.status(Response.Status.CONFLICT)
+				.entity(new CallStateExceptionDtoJaxb(ex.getClientId(), ex.getCallId()))
+				.build();
+	}
+
 
 }
