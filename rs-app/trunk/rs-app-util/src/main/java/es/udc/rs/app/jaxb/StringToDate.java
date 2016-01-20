4415c41ddc481c3615fb08c952a754acdd709632
o.blanco <o.blanco@95be2163-d335-43e2-8a67-876141709623>
2016-01-20 19:30:41 +0000

    move date conversors to util, to use it on client too
    git-svn-id: https://svn.fic.udc.es/grao4/ia/15-16/ia012@71 95be2163-d335-43e2-8a67-876141709623

--- rs-app/trunk/rs-app-util/src/main/java/es/udc/rs/app/jaxb/StringToDate.java
+++ rs-app/trunk/rs-app-util/src/main/java/es/udc/rs/app/jaxb/StringToDate.java
@@ -1,18 +1,29 @@
 package es.udc.rs.app.jaxb;
 
+import java.text.SimpleDateFormat;
 import java.util.Calendar;
 
+import es.udc.ws.util.exceptions.InputValidationException;
+
 public final class StringToDate {
+	public static final String DATE_FORMAT_MIN = "yyyy-MM-dd HH:mm:ss";
+	public static final String DATE_FORMAT_DAY = "yyyy-MM-dd HH:mm:ss";
 	
-	
-	public static Calendar parseStringToDate(String date) {
-		
-		String [] elem = date.split("[- :]");
+	public static Calendar getCalendar(String date) throws InputValidationException{
 		Calendar cal = Calendar.getInstance();
-		cal.set(Integer.parseInt(elem[0]), Integer.parseInt(elem[1]), 
-				Integer.parseInt(elem[2]), Integer.parseInt(elem[3]), Integer.parseInt(elem[4]),
-				Integer.parseInt(elem[5]));
+		SimpleDateFormat sdf = new SimpleDateFormat(DATE_FORMAT_MIN);
+		try {
+			cal.setTime(sdf.parse(date));
+		} catch (Exception e) {
+			throw new InputValidationException("Formato de fecha incorrecto");
+		}
+		
 		return cal;
+	}
+	
+	public static String getDateString(Calendar cal) {
+		SimpleDateFormat format = new SimpleDateFormat(DATE_FORMAT_MIN);
+		return format.format(cal.getTime());
 	}
 
 }
